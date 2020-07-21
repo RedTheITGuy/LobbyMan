daa3baf2c957d81b1b44175334c8a54cca6bcbb9
Red the IT Guy <redtheitguy@gmail.com>
2020-07-21 14:22:00 +0100

    Added config

--- LobbyMan/src/main/java/me/RedTheITGuy/LobbyMan/Main.java
+++ LobbyMan/src/main/java/me/RedTheITGuy/LobbyMan/Main.java
@@ -4,6 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		// Creates the config if it doesn't exist
+		this.saveDefaultConfig();
+		// Registers the event listener
 		getServer().getPluginManager().registerEvents(new EventListener(), this);
 	}
 }
