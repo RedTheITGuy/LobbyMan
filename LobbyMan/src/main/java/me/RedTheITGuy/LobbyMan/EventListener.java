4f8b2439daf8733f6fc00e614a6b12bb7e3dee1c
Red the IT Guy <redtheitguy@gmail.com>
2020-07-21 14:20:29 +0100

    Added event listener

---
+++ LobbyMan/src/main/java/me/RedTheITGuy/LobbyMan/EventListener.java
@@ -0,0 +1,6 @@
+package me.RedTheITGuy.LobbyMan;
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener {
+	
+}
--- LobbyMan/src/main/java/me/RedTheITGuy/LobbyMan/Main.java
+++ LobbyMan/src/main/java/me/RedTheITGuy/LobbyMan/Main.java
@@ -4,6 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		
+		getServer().getPluginManager().registerEvents(new EventListener(), this);
 	}
 }
